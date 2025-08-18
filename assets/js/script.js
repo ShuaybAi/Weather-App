39181dc9d6ca3131a40e434e4083239b48bb6722
RosieTrainor <trainor.rosie@gmail.com>
2025-08-18 16:33:13 +0100

    Amend getWeekly/HourlyWeather funcs to return array of objects instead of array of arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,21 +9,21 @@ let countryCode = "GB";
 // Geocoding API Call
 /** Returns the lat and lon of a city */
 export async function getLatLon() {
-  try {
-    const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&limit=1&appid=${apiKey}`
-    const response = await fetch(geoURL);
-    if (!response.ok) {
-      const errorData = await response.json();
-      console.error('API error response:', errorData);
-      throw new Error('API failed');
-    };
-    const data = await response.json();
-    let cityLat = data[0].lat;
-    let cityLon = data[0].lon;
-    return [cityLat, cityLon];
-  } catch (err) {
-    console.error('Error:', err);
-  }
+    try {
+        const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&limit=1&appid=${apiKey}`
+        const response = await fetch(geoURL);
+        if (!response.ok) {
+            const errorData = await response.json();
+            console.error('API error response:', errorData);
+            throw new Error('API failed');
+        };
+        const data = await response.json();
+        let cityLat = data[0].lat;
+        let cityLon = data[0].lon;
+        return [cityLat, cityLon];
+    } catch (err) {
+        console.error('Error:', err);
+    }
 }
 
 // Weather API Call
@@ -48,45 +48,46 @@ async function getWeather(lat, lon) {
  * the temp, the description, and the id for the rest of the day*/
 function getHourlyWeather(data) {
     const today = new Date().toISOString().slice(0, 10);
-    const time = [];
-    const temps = [];
-    const description = [];
-    const weatherId = [];
+    const hourlyArray = [];
     data.list.forEach(entry => {
         if (entry.dt_txt.split(" ")[0] === today) {
-            time.push(entry.dt_txt.split(" ")[1]);
-            temps.push(entry.main.temp);
-            description.push(entry.weather[0].description);
-            weatherId.push(entry.weather[0].id)
+            hourlyArray.push({
+                time: entry.dt_txt.split(" ")[1],
+                temp: entry.main.temp,
+                description: entry.weather[0].description,
+                weatherId: entry.weather[0].id,
+            })
         }
     });
-    return [time, temps, description, weatherId];
+    return hourlyArray;
 }
 
 /** Takes the weather from 12:00:00 each day of the forecast and returns an array of 4 arrays for the date of each forecast, 
  * the temp, the description, and the id* */
-function getWeeksWeather(data) {
+function getWeeklyWeather(data) {
     const today = new Date().toISOString().slice(0, 10);
-    const date = [];
-    const temps = [];
-    const description = [];
-    const weatherId = [];
-    // First item = today's weather at the time of the call
-    date.push(data.list[0].dt_txt.split(" ")[0]);
-    temps.push(data.list[0].main.temp);
-    description.push(data.list[0].weather[0].description);
-    weatherId.push(data.list[0].weather[0].id);
+    const weeklyArray = [];
+    // First object = today's weather at the time of the call
+    weeklyArray.push({
+        date: data.list[0].dt_txt.split(" ")[0],
+        temp: data.list[0].main.temp,
+        description: data.list[0].weather[0].description,
+        weatherId: data.list[0].weather[0].id,
+    })
+
     // then next weeks weather at 12:00
     data.list.forEach(entry => {
         const [entryDate, entryTime] = entry.dt_txt.split(" ");
         if (entryDate !== today && entryTime === "12:00:00") {
-            date.push(entry.dt_txt.split(" ")[0]);
-            temps.push(entry.main.temp);
-            description.push(entry.weather[0].description);
-            weatherId.push(entry.weather[0].id)
+            weeklyArray.push({
+            date: entry.dt_txt.split(" ")[0],
+            temp: entry.main.temp,
+            description: entry.weather[0].description,
+            weatherId: entry.weather[0].id
+            })
         }
     })
-    return [date, temps, description, weatherId];
+    return weeklyArray;
 }
 
 
@@ -99,7 +100,7 @@ async function forecast() {
     const [lat, lon] = await getLatLon();
     const weatherData = await getWeather(lat, lon);
     const hourlyWeather = getHourlyWeather(weatherData);
-    const weeklyWeather = getWeeksWeather(weatherData);
+    const weeklyWeather = getWeeklyWeather(weatherData);
     console.log(hourlyWeather);
     console.log(weeklyWeather);
 }
