a6418eab4c6a2595d0c766715cbd29a38da868c5
Oliver Lacey <olly.john@live.co.uk>
2025-08-18 15:12:53 +0100

    Add map initialization and weather layer functionality

--- assets/js/map.js
+++ assets/js/map.js
@@ -7,3 +7,63 @@ import { apiKey } from "./script.js";
 mapboxgl.accessToken =
 "pk.eyJ1IjoiYWVzb20iLCJhIjoiY21lZzEzdHM4MHVzdjJqc2Y4cGtrMzJseSJ9.7j6FIpAePZGj6gOidJ35Hw";
 
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/standard", 
+  projection: "globe", 
+  zoom: 1.2, 
+  center: [30, 15], 
+  scrollZoom: true,
+  boxZoom: true
+});
+
+map.addControl(new mapboxgl.NavigationControl());
+
+
+
+async function showLatLon() {
+  const coords = await getLatLon();
+  map.flyTo({
+          center: [coords[1], coords[0]],
+          zoom: 12,
+          speed: 3,
+          curve: 1.2,
+        });
+}
+
+showLatLon();
+
+
+map.on("style.load", () => {
+    map.setFog({});
+
+    map.addSource('weatherLayer', {
+        type: 'raster',
+        tiles: [
+            `https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=${apiKey}`
+        ],
+        tileSize: 256,
+        attribution: 'Weather © OpenWeather'
+    });
+});
+
+map.on('load', () => {
+  checkbox.addEventListener('change', (e) => {
+    if (!map.getLayer('weatherLayer')) {
+      map.addLayer({
+        id: 'weatherLayer',
+        type: 'raster',
+        source: 'weatherLayer',
+        paint: {
+          'raster-opacity': 0,
+          'raster-opacity-transition': { duration: 500 }
+        }
+      });
+    }
+    map.setPaintProperty(
+      layerId,
+      'raster-opacity',
+      e.target.checked ? 1 : 0
+    );
+  });
+});
