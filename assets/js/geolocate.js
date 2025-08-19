517f73f1a8d64cc878dc8eef0c40732ebb8d032e
Oliver Lacey <olly.john@live.co.uk>
2025-08-19 12:46:50 +0100

    Fix geolocation functionality and include geolocate.js in index.html

--- assets/js/geolocate.js
+++ assets/js/geolocate.js
@@ -1,17 +1,20 @@
-const locate = (document.getElementById = "geolocate");
 
-locate.addEventListener("click,", () => {
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        return latitude, longitude;
-      },
-      (error) => {
-        output.textContent = `Error: ${error.message}`;
-      }
-    );
-  } else {
-    output.textContent = "Geolocation is not supported in this browser.";
-  }
-});
+  const locate = document.getElementById("geolocate");
+
+  locate.addEventListener("click", () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const geoLat = position.coords.latitude;
+          const geoLon = position.coords.longitude;
+          console.log(geoLat,geoLon)
+        },
+        (error) => {
+          output.textContent = `Error: ${error.message}`;
+        }
+      );
+    } else {
+      output.textContent = "Geolocation is not supported in this browser.";
+    }
+  });
+
